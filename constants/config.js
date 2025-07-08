8510426f94425244673f908183e98251156dfd59
Priyanshu Shishodia <priyanshushishodia008@gmail.com>
2025-07-08 23:02:05 +0530

    change

--- constants/config.js
+++ constants/config.js
@@ -1,16 +1,24 @@
-const  CONNECTED_TOKEN = "connected_token"
+import dotenv from "dotenv";
+dotenv.config();
+
+const CONNECTED_TOKEN = "connected_token";
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:4173",
+];
+
+if (process.env.CLIENT_URL) {
+  allowedOrigins.push(process.env.CLIENT_URL);
+}
 
 const corsOptions = {
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
-      process.env.CLIENT_URL,
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  };
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
 
-export{
-    CONNECTED_TOKEN,
-    corsOptions
-}
+export {
+  CONNECTED_TOKEN,
+  corsOptions,
+};
